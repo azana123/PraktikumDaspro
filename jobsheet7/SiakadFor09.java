ed0b8ccc8cf91acc52134fe09f39286a1ca0b779
Fafiq_Azana <azanalutfi321@gmail.com>
2025-10-25 10:29:18 +0700

    percobaan 1

--- jobsheet7/SiakadFor09.java
+++ jobsheet7/SiakadFor09.java
@@ -4,10 +4,10 @@ public class SiakadFor09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 100, terendah = 0;
+        double nilai, tertinggi = 0, terendah = 100;
 
         for (int i = 1; i <= 10; i++){
-            System.out.print("Masukkan nilai mahasiswa ke-" + i + ":");
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
             nilai = sc.nextDouble();
 
             if (nilai > tertinggi) {
