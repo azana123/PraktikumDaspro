b646c2530b0ce2347cd075c81ff6a6980300f3f7
Fafiq_Azana <azanalutfi321@gmail.com>
2025-11-10 09:53:33 +0700

    Tugas 3

---
+++ Jobsheet9/tugas3.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class tugas3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String[] menu = {
+            "Nasi Goreng", 
+            "Mie Goreng", 
+            "Roti Bakar", 
+            "Kentang Goreng", 
+            "Teh Tarik", 
+            "Cappucino", 
+            "Chocolate Ice"
+        };
+
+        System.out.println("=== Daftar Menu Kafe ===");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println((i + 1) + ". " + menu[i]);
+        }
+
+        System.out.print("\nMasukkan nama makanan/minuman yang ingin dicari: ");
+        String cari = input.nextLine();
+
+        boolean ditemukan = false;
+        for (int i = 0; i < menu.length; i++) {
+            if (menu[i].equalsIgnoreCase(cari)) { 
+                ditemukan = true;
+                System.out.println("\n " + menu[i] + " tersedia di menu (posisi ke-" + (i + 1) + ").");
+                break;
+            }
+        }
+
+        if (!ditemukan) {
+            System.out.println("\n Maaf, " + cari + " tidak tersedia di menu.");
+        }
+
+        input.close();
+    }
+}
+
