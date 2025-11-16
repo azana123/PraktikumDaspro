88e8b2cca222d121b44c8fc0c8d4067a0c4aa88f
Fafiq_Azana <azanalutfi321@gmail.com>
2025-11-16 15:33:22 +0700

    Percobaan 2

---
+++ Jobsheet10/BioskopWithScanner09.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BioskopWithScanner09 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int baris, kolom;
+        String nama, next;
+        String[][] penonton = new String [4][2];
+
+        while (true) {
+            System.out.print("Masukan nama: ");
+            nama = sc.nextLine();
+            System.out.print("Masukan baris: ");
+            baris = sc.nextInt();
+            System.out.print("Masukan kolom: ");
+            kolom = sc.nextInt();
+            sc.nextLine();
+
+            penonton[baris-1][kolom-1] = nama;
+
+            System.out.print("Input penonton lainnya? (y/nn): ");
+            next = sc.nextLine();
+
+            if (next.equalsIgnoreCase("n")) {
+                break;
+            }
+        }
+    }
+}
