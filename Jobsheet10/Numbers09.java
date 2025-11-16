75162924891526301ff78b56011dfccb6630ae41
Fafiq_Azana <azanalutfi321@gmail.com>
2025-11-16 16:24:20 +0700

    Modifikasi Percobaan 3

--- Jobsheet10/Numbers09.java
+++ Jobsheet10/Numbers09.java
@@ -1,9 +1,19 @@
+import java.util.Arrays;
+
 public class Numbers09 {
     public static void main(String[] args) {
         
-        int[][] myNumber = new int[3][];
-        myNumber[0] = new int[5];
-        myNumber[1] = new int[3];
-        myNumber[2] = new int[1];
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[3];
+        myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+        }
     }
 }
