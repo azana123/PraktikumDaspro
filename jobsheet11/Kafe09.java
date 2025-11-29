b5f477d6e8cf9d6a60605bb0aa6bac870d5f8550
Fafiq_Azana <azanalutfi321@gmail.com>
2025-11-29 22:45:32 +0700

    Modifikasi Percobaan 2

--- jobsheet11/Kafe09.java
+++ jobsheet11/Kafe09.java
@@ -1,11 +1,19 @@
 package PraktikumDaspro.jobsheet11;
 
 public class Kafe09 {
-    public static void menu(String namaPelanggan, boolean isMember) {
+    public static void menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat datang, " + namaPelanggan);
 
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
+        if (kodePromo.equals("DISKON50")) {
+            System.out.println("Kode promo valid!, Anda mendapat diskon 50%");
+        }else if (kodePromo.equals("DISKON30")) {
+            System.out.println("Kode promo valid!, Anda mendapat diskon 30%");
+        }else {
+            System.out.println("Kode promo tidak tidak tersedia");
         }
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
@@ -18,6 +26,6 @@ public class Kafe09 {
         System.out.println("Silakan pilih menu yang Anda inginkan");
     }
     public static void main(String[] args) {
-        menu("Andri", true);
+        menu("Andri", true, "DISKON50");
     }
 }
